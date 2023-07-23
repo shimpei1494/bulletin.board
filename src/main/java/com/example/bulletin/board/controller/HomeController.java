d2afeeee6f74053b825424d060257e2329701d48
大下慎平 <one1218all38@icloud.com>
2023-07-23 21:36:34 +0900

    データベース設定し、アプリ起動できるところまで

---
+++ src/main/java/com/example/bulletin/board/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.bulletin.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+}
