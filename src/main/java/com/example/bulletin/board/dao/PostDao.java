dd7f061b0b7fa7690340f531762494aff6902795
大下慎平 <one1218all38@icloud.com>
2023-08-15 16:45:40 +0900

    微修正

--- src/main/java/com/example/bulletin/board/dao/PostDao.java
+++ src/main/java/com/example/bulletin/board/dao/PostDao.java
@@ -1,11 +1,9 @@
 package com.example.bulletin.board.dao;
 
 import com.example.bulletin.board.entity.gen.Post;
-import com.example.bulletin.board.entity.gen.PostExample;
 import com.example.bulletin.board.entity.gen.PostKey;
 import com.example.bulletin.board.mapper.gen.PostMapper;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class PostDao {
@@ -16,10 +14,6 @@ public class PostDao {
     }
 
     public Post selectPk(int id) {
-        // 以下不要だった
-//        PostExample example = new PostExample();
-//        PostExample.Criteria criteria = example.createCriteria();
-//        criteria.andIdEqualTo(id);
         PostKey key = new PostKey();
         key.setId(id);
         return postMapper.selectByPrimaryKey(key);
