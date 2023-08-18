efc73461ee4e5ac072aa825a8549c3265be22bdb
大下慎平 <one1218all38@icloud.com>
2023-08-18 13:38:22 +0900

    JdbcUserDetailsManagerを利用してユーザー認証

--- src/main/java/com/example/bulletin/board/config/SecurityConfig.java
+++ src/main/java/com/example/bulletin/board/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.bulletin.board.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +9,19 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig{
+
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -40,13 +48,31 @@ public class SecurityConfig{
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password(passwordEncoder().encode("123456"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsManager userDetailsManager() {
+        JdbcUserDetailsManager user = new JdbcUserDetailsManager(this.dataSource);
+        // ユーザーを追加したい時
+//        user.createUser(makeUser("bbb", "pass", "USER"));
+
+        return user;
     }
+
+    private UserDetails makeUser(String user, String pass, String role) {
+        return User.withUsername(user)
+                .password(passwordEncoder().encode(pass))
+                .roles(role)
+                .disabled(false)
+                .build();
+    }
+
+    // データベースを使用せずユーザー名user、パス123456という固定でログインするための設定
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password(passwordEncoder().encode("123456"))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
 }
